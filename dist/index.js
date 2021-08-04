2f8db868aa51c17d96bdc63979d7e4b1dc63813d
Dave Lilly <dave@lillys.org>
2021-08-04 12:57:03 -0500

    updates

---
+++ dist/index.js
@@ -0,0 +1,82 @@
+const { gql } = require('@apollo/client/core');
+const commonFields = `
+    id
+    name
+    slug
+`;
+const meta = `
+    meta {
+        total
+        limit
+        count
+        offset
+    }
+`;
+const productFields = `
+    ${commonFields}
+    longDescription
+    categories {
+        ${commonFields}
+    }
+    variants {
+        sku
+        prices {
+            list
+        }
+        images {
+            url
+        }
+    }
+`;
+const CATEGORY_HIERARCHY_QUERY = gql `    
+    query {
+        categoryHierarchy {
+            ${commonFields}
+            children {
+                ${commonFields}
+                children {
+                    ${commonFields}
+                }
+            }
+        }
+    }
+`;
+const PRODUCTS_QUERY = args => gql `
+    query productsQuery {
+        ${args.keyword ? `products(keyword:"${args.keyword}")` : `products`} {
+            ${meta}
+            results {
+                ${productFields}
+            }
+        }
+    }
+`;
+const lookupArgs = args => {
+    if (args.id) {
+        return `id:"${args.id}"`;
+    }
+    else if (args.sku) {
+        return `sku:"${args.sku}"`;
+    }
+    else if (args.slug) {
+        return `slug:"${args.slug}"`;
+    }
+    else {
+        throw new Error("id, sku, or slug must be specified");
+    }
+};
+const PRODUCT_QUERY = args => gql `
+    query productQuery {
+        product(${lookupArgs(args)}) {
+            ${productFields}
+        }
+    }
+`;
+const CATEGORY_QUERY = args => gql `
+    query categoryQuery {
+        category(${lookupArgs(args)}) {
+            ${commonFields}
+        }
+    }
+`;
+module.exports = { CATEGORY_HIERARCHY_QUERY, CATEGORY_QUERY, PRODUCTS_QUERY, PRODUCT_QUERY };
