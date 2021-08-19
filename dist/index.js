e71cd5a04b16cd8dad03e2aee9f43a0182a81e4d
Dave Lilly <dave@lillys.org>
2021-08-19 11:48:20 -0500

    oops forgot to run build! silly :)

--- dist/index.js
+++ dist/index.js
@@ -13,140 +13,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.searchProducts = exports.queryProducts = exports.fetchAllProducts = exports.fetchProduct = exports.CATEGORY_QUERY = exports.PRODUCT_QUERY = exports.PRODUCTS_QUERY = exports.CATEGORY_HIERARCHY_QUERY = void 0;
 require("reflect-metadata");
-const { gql } = require('@apollo/client/core');
-// const GraphQL = require('./graphql-client')
 const graphql_client_1 = __importDefault(require("./graphql-client"));
-const commonFields = `
-    id
-    name
-    slug
-`;
-const meta = `
-    meta {
-        total
-        limit
-        count
-        offset
-    }
-`;
-const productFields = `
-    ${commonFields}
-    longDescription
-    categories {
-        ${commonFields}
-    }
-    variants {
-        sku
-        articleNumberMax: attribute(name: "articleNumberMax")
-        prices {
-            list
-            sale
-        }
-        images {
-            url
-        }
-    }
-`;
-exports.CATEGORY_HIERARCHY_QUERY = gql `    
-    query {
-        categoryHierarchy {
-            ${commonFields}
-            children {
-                ${commonFields}
-                children {
-                    ${commonFields}
-                }
-            }
-        }
-    }
-`;
-const PRODUCTS_QUERY = (gqlc) => {
-    var _a, _b;
-    return gql `
-    query productsQuery {
-        ${((_a = gqlc.args) === null || _a === void 0 ? void 0 : _a.keyword) ? `products(keyword:"${(_b = gqlc.args) === null || _b === void 0 ? void 0 : _b.keyword}")` : `products`} {
-            ${meta}
-            results {
-                ${productFields}
-            }
-        }
-    }
-`;
-};
-exports.PRODUCTS_QUERY = PRODUCTS_QUERY;
-const lookupArgs = args => {
-    if (args.id) {
-        return `id:"${args.id}"`;
-    }
-    else if (args.sku) {
-        return `sku:"${args.sku}"`;
-    }
-    else if (args.slug) {
-        return `slug:"${args.slug}"`;
-    }
-    else {
-        throw new Error("id, sku, or slug must be specified");
-    }
-};
-const PRODUCT_QUERY = (gqlc) => gql `
-    query productQuery {
-        product(${lookupArgs(gqlc.args)}) {
-            ${productFields}
-        }
-    }
-`;
-exports.PRODUCT_QUERY = PRODUCT_QUERY;
-const CATEGORY_QUERY = (gqlc) => gql `
-    query categoryQuery {
-        category(${lookupArgs(gqlc.args)}) {
-            ${commonFields}
-            products {
-                ${productFields}
-            }
-        }
-    }
-`;
-exports.CATEGORY_QUERY = CATEGORY_QUERY;
-async function fetchProduct(gqlc) {
-    return graphql_client_1.default(gqlc.graphqlConfig).query({ query: exports.PRODUCT_QUERY(gqlc) }).then(x => x.data.product);
-}
-exports.fetchProduct = fetchProduct;
-async function fetchAllProducts(gqlc) {
-    return graphql_client_1.default(gqlc.graphqlConfig).query({ query: exports.PRODUCTS_QUERY(gqlc) }).then(x => x.data.products);
-}
-exports.fetchAllProducts = fetchAllProducts;
-// export async function fetchProducts(ids: String[], cmsContext?: CmsContext, graphqlConfig?: GraphqlConfig): Promise<any> {
-//     try {
-//         let graphqlClient = GraphQL(graphqlConfig)
-//         let x = await Promise.all(ids.map(async prodId => {
-//             return await fetchProduct({ id: prodId }, cmsContext, graphqlConfig)
-//         }))
-//         return new Promise((resolve, reject) => { resolve(x) })
-//     }
-//     catch (e) {
-//         console.error(`Error: ${e}`)
-//     }
-// }
-async function queryProducts(gqlc) {
-    gqlc.args.full = gqlc.args.full || false;
-    return graphql_client_1.default(gqlc.graphqlConfig).query({ query: exports.CATEGORY_QUERY(gqlc) }).then(x => x.data.category);
-}
-exports.queryProducts = queryProducts;
-async function searchProducts(gqlc) {
-    return graphql_client_1.default(gqlc.graphqlConfig).query({ query: exports.PRODUCTS_QUERY(gqlc) }).then(x => x.data.products.results);
-}
-exports.searchProducts = searchProducts;
+const queries_1 = require("./queries");
 __exportStar(require("./types"), exports);
-// import { Category, Product, Prices, ProductImage, ResultsMeta, ProductResults, CategoryResults, Attribute, Variant } from './types'
-// export { Category, Product, Prices, ProductImage, ResultsMeta, ProductResults, CategoryResults, Attribute, Variant }
-// module.exports = { 
-//     CATEGORY_HIERARCHY_QUERY, 
-//     CATEGORY_QUERY, 
-//     PRODUCTS_QUERY, 
-//     PRODUCT_QUERY,
-//     fetchProduct,
-//     Category,
-//     Product
-// }
+function default_1(graphqlConfig) {
+    let client = graphql_client_1.default(graphqlConfig);
+    return {
+        fetchAllProducts: async function (args) {
+            return (await client.query({ query: queries_1.productsQuery(args) })).data.products;
+        },
+        fetchProduct: async function (args) {
+            return (await client.query({ query: queries_1.productQuery(args) })).data.product;
+        },
+        fetchCategoryHierarchy: async function () {
+            return (await client.query({ query: queries_1.categoryHierarchyQuery })).data.categoryHierarchy;
+        },
+        fetchCategory: async function (args) {
+            return (await client.query({ query: queries_1.categoryQuery(args) })).data.category;
+        }
+    };
+}
+exports.default = default_1;
--- dist/test.js
+++ dist/test.js
@@ -1,11 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 const _ = require('lodash');
 const async = require('async');
 const assert = require('assert');
 const chalk = require('chalk');
 const nconf = require('nconf');
 const axios = require('axios');
-const { fetchAllProducts } = require('./index');
-const { ProductResults } = require('./types');
+const index_1 = __importDefault(require("./index"));
 let backendKeys = [];
 // command line arguments
 const graphqlHost = nconf.argv().get('graphqlHost') || `http://localhost:3002`;
@@ -15,8 +19,9 @@ let run = async () => {
     console.log();
     let bkResponse = await axios.get(`${graphqlHost}/keys`);
     backendKeys = bkResponse.data.keys;
+    backendKeys = [_.first(backendKeys)];
     async.eachSeries(backendKeys, async (backendKey) => {
-        let graphqlConfig = { graphqlUrl, backendKey };
+        let client = index_1.default({ graphqlUrl, backendKey });
         let mapped = {};
         let flattenCategories = cats => {
             _.each(cats, cat => {
@@ -28,30 +33,30 @@ let run = async () => {
         console.log(chalk.gray(backendKey));
         console.log('-'.padStart(backendKey.length, '-'));
         console.log();
-        let productsResponse = await fetchAllProducts({ graphqlConfig });
+        let productsResponse = await client.fetchAllProducts();
         console.log(`    products:         [ ${chalk.magenta(productsResponse.meta.total)} ]`);
-        // let topLevelCategories = (await client.query({ query: CATEGORY_HIERARCHY_QUERY })).data.categoryHierarchy
-        // flattenCategories(topLevelCategories)
-        // let secondLevelCategories = _.flatten(_.map(topLevelCategories, 'children'))
-        // let thirdLevelCategories = _.flatten(_.map(secondLevelCategories, 'children'))
-        // console.log(`    categories (top): [ ${chalk.magenta(topLevelCategories.length)} ]`)
-        // console.log(`               (2nd): [ ${chalk.magenta(secondLevelCategories.length)} ]`)
-        // console.log(`               (3rd): [ ${chalk.magenta(thirdLevelCategories.length)} ]`)
-        // console.log()
-        // let product = _.sample(productsResponse.results)
-        // let category = _.sample(Object.values(mapped))
-        // process.stdout.write(`    lookup category [ id: ${chalk.yellow(category.id)} ]...`)
-        // let categoryById = (await client.query({ query: CATEGORY_QUERY({ id: category.id }) })).data.category
-        // assert.deepStrictEqual(categoryById, _.omit(category, 'children'))
-        // console.log(`[ ${chalk.green('pass')} ]`)
-        // process.stdout.write(`    lookup category [ slug: ${chalk.yellow(category.slug)} ]...`)
-        // let categoryBySlug = (await client.query({ query: CATEGORY_QUERY({ slug: category.slug }) })).data.category
-        // assert.deepStrictEqual(categoryBySlug, _.omit(category, 'children'))
-        // console.log(`[ ${chalk.green('pass')} ]`)
-        // process.stdout.write(`    lookup product  [ id: ${chalk.yellow(product.id)} ]...`)
-        // let productById = (await client.query({ query: PRODUCT_QUERY({ id: product.id }) })).data.product
-        // assert.deepStrictEqual(_.omit(productById, 'categories'), _.omit(product, 'categories'))
-        // console.log(`[ ${chalk.green('pass')} ]`)
+        let product = _.sample(productsResponse.results);
+        let topLevelCategories = await client.fetchCategoryHierarchy();
+        flattenCategories(topLevelCategories);
+        let secondLevelCategories = _.flatten(_.map(topLevelCategories, 'children'));
+        let thirdLevelCategories = _.flatten(_.map(secondLevelCategories, 'children'));
+        console.log(`    categories (top): [ ${chalk.magenta(topLevelCategories.length)} ]`);
+        console.log(`               (2nd): [ ${chalk.magenta(secondLevelCategories.length)} ]`);
+        console.log(`               (3rd): [ ${chalk.magenta(thirdLevelCategories.length)} ]`);
+        console.log();
+        let category = _.sample(Object.values(mapped));
+        process.stdout.write(`    lookup category [ id: ${chalk.yellow(category.id)} ]...`);
+        let categoryById = await client.fetchCategory({ id: category.id });
+        assert.deepStrictEqual(_.omit(categoryById, ['children', 'products']), _.omit(category, ['children', 'products']));
+        console.log(`[ ${chalk.green('pass')} ]`);
+        process.stdout.write(`    lookup category [ slug: ${chalk.yellow(category.slug)} ]...`);
+        let categoryBySlug = await client.fetchCategory({ slug: category.slug });
+        assert.deepStrictEqual(_.omit(categoryBySlug, ['children', 'products']), _.omit(category, ['children', 'products']));
+        console.log(`[ ${chalk.green('pass')} ]`);
+        process.stdout.write(`    lookup product  [ id: ${chalk.yellow(product.id)} ]...`);
+        let productById = await client.fetchProduct({ id: product.id });
+        assert.deepStrictEqual(_.omit(productById, 'categories'), _.omit(product, 'categories'));
+        console.log(`[ ${chalk.green('pass')} ]`);
     });
 };
 run();
